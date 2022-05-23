5ac5039d947e1a86cc93e79a1b2ba08204658bce
shaulov <shineginn@gmail.com>
2022-05-23 10:35:34 +0300

    Add random character generation and functions for handling api data

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,6 +2,9 @@ class MarvelService {
     #apiBase = 'https://gateway.marvel.com:443/v1/public/';
     #apiKey = 'apikey=79aa42515873b8889610444e14ec4446';
 
+    DESCRIPTION_PLACEHOLDER = 'Character has no description';
+    DESCRIPTION_LENGTH = 210;
+
     getResource = async (url) => {
         let result = await fetch(url);
 
@@ -12,12 +15,27 @@ class MarvelService {
         return await result.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this.#apiBase}characters?limit=9&offset=210&${this.#apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this.#apiBase}characters?limit=9&offset=210&${this.#apiKey}`);
+        return res.data.results.map(this.#transformCharacter);
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`${this.#apiBase}characters/${id}?${this.#apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this.#apiBase}characters/${id}?${this.#apiKey}`);
+        return this.#transformCharacter(res.data.results[0]);
+    }
+
+    #transformCharacter = (char) => {
+
+        let description = char.description ? `${char.description.substring(0, this.DESCRIPTION_LENGTH)}...` : this.DESCRIPTION_PLACEHOLDER;
+
+        return {
+            name: char.name,
+            description: description,
+            thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+        };
     }
 }
 
