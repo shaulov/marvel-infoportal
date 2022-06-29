564d7a3c48d425a6a5351d991cca9bb75e40b18d
shaulov <shineginn@gmail.com>
2022-06-29 16:29:36 +0300

    Add search form

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,36 +1,42 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
+import RandomChar from '../randomChar/RandomChar';
+import CharList from '../charList/CharList';
+import CharInfo from '../charInfo/CharInfo';
+import CharSearchForm from '../charSearchForm/CharSearchForm.js';
 
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import ErrorBoundary from '../errorBoundary/ErrorBoundary';
 
 import decoration from '../../resources/img/vision.png';
 
 const MainPage = () => {
-    const [selectedChar, setSelectedChar] = useState(null);
+	const [selectedChar, setSelectedChar] = useState(null);
 
-    const onCharSelected = (id) => {
-        setSelectedChar(id);
-    }
+	const onCharSelected = (id) => {
+		setSelectedChar(id);
+	};
 
-    return (
-        <>
-            <ErrorBoundary>
-                <RandomChar/>
-            </ErrorBoundary>
-            <div className="char__content">
-                <ErrorBoundary>
-                    <CharList onCharSelected={onCharSelected}/>
-                </ErrorBoundary>
-                <ErrorBoundary>
-                    <CharInfo charId={selectedChar}/>
-                </ErrorBoundary>
-            </div>
-            <img className="bg-decoration" src={decoration} alt="vision"/>
-        </>
-    )
-}
+	return (
+		<>
+			<ErrorBoundary>
+				<RandomChar />
+			</ErrorBoundary>
+			<div className="char__content">
+				<ErrorBoundary>
+					<CharList onCharSelected={onCharSelected} />
+				</ErrorBoundary>
+				<div>
+					<ErrorBoundary>
+						<CharInfo charId={selectedChar} />
+					</ErrorBoundary>
+					<ErrorBoundary>
+						<CharSearchForm />
+					</ErrorBoundary>
+				</div>
+			</div>
+			<img className="bg-decoration" src={decoration} alt="vision" />
+		</>
+	);
+};
 
 export default MainPage;
