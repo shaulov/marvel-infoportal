5019c1a703f554274306f6a890ce4e10b6747913
shaulov <shineginn@gmail.com>
2022-06-30 15:52:07 +0300

    Add render component for pages

---
+++ src/components/pages/SinglePage.js
@@ -0,0 +1,57 @@
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+import useMarvelService from '../../../services/MarvelService';
+import Spinner from '../../spinner/Spinner';
+import ErrorMessage from '../../errorMessage/ErrorMessage';
+import AppBanner from '../../AppBanner/AppBanner';
+
+import '../singleComicPage/SingleComicPage';
+
+const SinglePage = ({ Component, dataType }) => {
+	const { id } = useParams();
+	const [data, setData] = useState(null);
+
+	const { loading, error, getCharacter, getComic, clearError } =
+		useMarvelService();
+
+	useEffect(() => {
+		updateData();
+	}, [id]);
+
+	const updateData = () => {
+		clearError();
+
+		switch (dataType) {
+			case 'comic':
+				getComic(id).then(onDataLoaded);
+				break;
+			case 'char':
+				getCharacter(id).then(onDataLoaded);
+				break;
+			default:
+				return null;
+		}
+	};
+
+	const onDataLoaded = (data) => {
+		setData(data);
+	};
+
+	const errorMessage = error ? <ErrorMessage /> : null;
+	const spinner = loading ? <Spinner /> : null;
+	const content = !(loading || error || !data) ? (
+		<Component comic={data} />
+	) : null;
+
+	return (
+		<>
+			<AppBanner />
+			{errorMessage}
+			{spinner}
+			{content}
+		</>
+	);
+};
+
+export default SinglePage;
