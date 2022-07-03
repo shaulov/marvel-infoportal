3d0f29503aadd5a93aa42142c072d1c41a0277e6
shaulov <shineginn@gmail.com>
2022-07-03 17:28:31 +0300

    Fix hooks and services

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -1,8 +1,6 @@
 import { useState, useCallback } from 'react';
 
 export const useHttp = () => {
-	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState(null);
 	const [process, setProcess] = useState('waiting');
 
 	const request = useCallback(
@@ -12,7 +10,6 @@ export const useHttp = () => {
 			body = null,
 			headers = { 'Content-Type': 'aplication/json' }
 		) => {
-			setLoading(true);
 			setProcess('loading');
 
 			try {
@@ -25,12 +22,8 @@ export const useHttp = () => {
 				}
 
 				const data = await response.json();
-
-				setLoading(false);
 				return data;
 			} catch (err) {
-				setLoading(false);
-				setError(err.message);
 				setProcess('error');
 				throw err;
 			}
@@ -39,9 +32,8 @@ export const useHttp = () => {
 	);
 
 	const clearError = useCallback(() => {
-		setError(null);
 		setProcess('loading');
 	}, []);
 
-	return { loading, request, error, clearError, process, setProcess };
+	return { request, clearError, process, setProcess };
 };
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,8 +1,7 @@
 import { useHttp } from '../hooks/http.hook';
 
 const useMarvelService = () => {
-	const { loading, request, error, clearError, process, setProcess } =
-		useHttp();
+	const { request, clearError, process, setProcess } = useHttp();
 
 	const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
 	const _apiKey = 'apikey=79aa42515873b8889610444e14ec4446';
@@ -86,8 +85,6 @@ const useMarvelService = () => {
 	};
 
 	return {
-		loading,
-		error,
 		getAllCharacters,
 		getCharacter,
 		getCharacterByName,
